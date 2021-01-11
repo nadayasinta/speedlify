4574c2ad1aa6e47f08def31cc162f9170f8c0e0e
nadayasinta <nada@alterra.id>
2021-01-11 10:29:23 +0700

    add url

--- _data/sites/test.js
+++ _data/sites/test.js
@@ -5,8 +5,14 @@ module.exports = {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://app.akunbank.com",
 		"https://app.akunbank.com/login",
 		"https://app.akunbank.com/register",
+		"https://app.akunbank.com/inputEmail",
+		"https://app.akunbank.com/otp",
+		"https://app.akunbank.com/registerPIN",
+		"https://app.akunbank.com/resetPassword",
+		"https://app.akunbank.com/bantuan",
+		"https://app.akunbank.com/syaratKetentuan",
+		"https://app.akunbank.com/kebijakanPrivasi",
 	]
 };
