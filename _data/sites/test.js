e5114bd8445262df0d974eefb885419fc061e7b0
nadayasinta <nada@alterra.id>
2021-01-11 10:11:08 +0700

    rename

--- _data/sites/test.js
+++ _data/sites/test.js
@@ -1,9 +1,10 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
+	name: "Test Speedlify", // optional, falls back to object key
+	description: "Test Speedlify",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
+	skip: false,
 	urls: [
 		"https://www.speedlify.dev/"
 	]
